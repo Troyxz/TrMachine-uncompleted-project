a5b28c1e41a80c05fb3900b7f8fc7d8eb70b4c97
Troyxz <118859061+Troyxz@users.noreply.github.com>
2025-10-31 11:09:06 +0000

    more code
    just more code lol

--- TrMachine.py
+++ TrMachine.py
@@ -1,12 +1,13 @@
-from colorama import init, Fore, Style
+from colorama import init, Fore, Style;
+from webbrowser import open;
 import time;
 import os;
 
 def cls():
-  os.system("cls")
+  os.system("cls");
 
 def cooldown(seconds):
-  time.sleep(seconds)
+  time.sleep(seconds);
 
 class Screen1:
   def choice1():
@@ -18,45 +19,94 @@ class Screen1:
         |____|   |__|  \____|__  (____  /\___  >___|  /__|___|  /\___  >          \/  ||  \/
                               \/     \/     \/     \/        \/     \/                \/
       """
-    print(Fore.GREEN + Style.BRIGHT + inicial_logo + Style.RESET_ALL)
+    print();
+    print(Fore.GREEN + Style.BRIGHT + inicial_logo + Style.RESET_ALL);
 
     print()
-    print(Fore.YELLOW + Style.BRIGHT + "[sb]: search in bible ... ")
-    print("[sc]: search in cathecism ... ")
-    print("[exit]: exit ... " + Style.RESET_ALL)
+    print(Fore.YELLOW + Style.BRIGHT + "[1]: open the holy terminal ... ");
+    print("[2]: enter our whatsapp group ... ");
+    print("[exit]: exit ... " + Style.RESET_ALL);
 
-    global choice; choice = input(Fore.GREEN + Style.BRIGHT + "[ ... " + Style.RESET_ALL)
+    global choice; choice = input(Fore.GREEN + Style.BRIGHT + "[ ... " + Style.RESET_ALL);
 
 class Screen2:
 
-  def bible_section():
-    print()
+  def the_holy_terminal(): # THE MOST IMPORTANT PART OF THE CODE . . .
+    global command_of_user; command_of_user = True; # ((incomplete)) . . .
 
-  def cathecism_section():
-    print()
-
-Screen1.choice1() #RUNNING THE MAIN CODE
+Screen1.choice1(); #RUNNING THE MAIN CODE
 
 if choice == "exit":
-  print()
-  print(Fore.YELLOW + Style.BRIGHT + "[ info: closing the system in 5 seconds ... ]" + Style.RESET_ALL)
-  cooldown(5)
-  exit()
+  number_of_loading = 5;
+  number_of_dots = 0;
+  dot = "((error: dots not defined, fix it u monkey))";
 
-elif choice == "sb":
-  print()
-  print(Fore.YELLOW + Style.BRIGHT + "[ info: Opening the bible section ... ]" + Style.RESET_ALL)
-  cooldown(2.5)
-  Screen2.bible_section()
+  for loading in range(5):
+    if number_of_dots != 3: number_of_dots += 1;
+    elif number_of_dots == 3: number_of_dots = 1;
+    else: number_of_dots = 0; 
 
-elif choice == "sc":
-  print()
-  print(Fore.YELLOW + Style.BRIGHT + "[ info: Opening the cathecis, section ... ]" + Style.RESET_ALL)
-  cooldown(2.5)
-  Screen2.cathecism_section()
+    if number_of_dots == 1: dot = " . ";
+    elif number_of_dots == 2: dot = " . . ";
+    elif number_of_dots == 3: dot = " . . . ";
+    else: dot = " ((error: your code is a chaos, lad)) ";
+
+    cls();
+    print(Fore.YELLOW + Style.BRIGHT + "[ info: closing the system in ", number_of_loading ," secconds", dot ,"]" + Style.RESET_ALL);
+    cooldown(1);
+
+    number_of_loading -= 1;
+
+  exit();
+
+elif choice == "1":
+  number_of_loading = 3;
+  number_of_dots = 0;
+  dot = "((error: dots not defined, fix it u monkey))";
+
+  for loading in range(3):
+    if number_of_dots != 3: number_of_dots += 1;
+    elif number_of_dots == 3: number_of_dots = 1;
+    else: number_of_dots = 0; 
+
+    if number_of_dots == 1: dot = " . ";
+    elif number_of_dots == 2: dot = " . . ";
+    elif number_of_dots == 3: dot = " . . . ";
+    else: dot = " ((error: your code is a chaos, lad)) ";
+
+    cls();
+    print(Fore.YELLOW + Style.BRIGHT + "[ info: opening the holy terminal in ", number_of_loading ," secconds", dot ,"]" + Style.RESET_ALL);
+    cooldown(1);
+
+    number_of_loading -= 1;
+
+  Screen2.the_holy_terminal();
+
+elif choice == "2":
+  number_of_loading = 3;
+  number_of_dots = 0;
+  dot = "((error: dots not defined, fix it u monkey))";
+
+  for loading in range(3):
+    if number_of_dots != 3: number_of_dots += 1;
+    elif number_of_dots == 3: number_of_dots = 1;
+    else: number_of_dots = 0; 
+
+    if number_of_dots == 1: dot = " . ";
+    elif number_of_dots == 2: dot = " . . ";
+    elif number_of_dots == 3: dot = " . . . ";
+    else: dot = " ((error: your code is a chaos, lad)) ";
+
+    cls();
+    print(Fore.YELLOW + Style.BRIGHT + "[ info: redirecting in ", number_of_loading ," secconds", dot ,"]" + Style.RESET_ALL);
+    cooldown(1);
+
+    number_of_loading -= 1;
+
+  open("https://chat.whatsapp.com/LoMbxPwhj6vGacSQIvGU2I?mode=wwt");
 
 else:
     print("put a valid answer.")
-    cooldown(2.5)
-    cls()
-    Screen1.choice1()
+    cooldown(2.5);
+    cls();
+    Screen1.choice1();
